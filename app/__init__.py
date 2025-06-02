acd071cd6ae309c594c0ec6898ed21128e0255f6
Rodrigo Santos <rodrigosdn@gmail.com>
2025-06-02 17:45:27 -0300

    feat: adicionado a origem do ambiente de produção

--- app/__init__.py
+++ app/__init__.py
@@ -11,7 +11,8 @@ def create_app(config_class=Config):
 
     allowed_origins = [
     "http://localhost:5173",
-    "https://desatinar.pythonanywhere.com"
+    "https://desatinar.pythonanywhere.com",
+    "https://emissao-certificados.netlify.app/",
 ]
 
     db.init_app(app)
